0917cdca628b6071fda8efad13da0d08e0d23924
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-06-09 00:15:18 +0530

    Add a some feture

--- script.js
+++ script.js
@@ -6,59 +6,83 @@ const displayInput = document.querySelector('.displayInput')
 const display = document.querySelector('.display')
 const equal = document.querySelector('.equal')
 
-let firstNum = ''
-let secondNum = ''
-let oprat = ''
+let currentNum = ''
+let prevNum = ''
+let operatorDisplay = ''
 
 number.forEach((number)=>{
     number.addEventListener('click', (e)=>{
         handleNumber(e.target.textContent)
-        displayInput.textContent = firstNum
+        displayInput.textContent = currentNum
     })
 })
 
-operator.forEach((e)=>{
-    e.addEventListener('click', (e)=>{
+operator.forEach((op)=>{
+    op.addEventListener('click', (e)=>{
         handleOperator(e.target.textContent)
-        displayInput.textContent = oprat
-    })
+            let total = operation(currentNum, operatorDisplay, prevNum)
+            displayInput.textContent = total
+            currentNum = total
+            currentNum = ''
+            operatorDisplay = e.target.textContent
+        })
 })
 
 equal.addEventListener('click', (e)=>{
-    return opration(firstNum, oprat, secondNum)
+    let total = operation(currentNum, operatorDisplay, prevNum)
+    displayInput.textContent = total
+    currentNum = total
+    // console.log(operation(prevNum, operatorDisplay, currentNum))
 })
 
 clear.addEventListener('click', ()=>{
-    display.textContent = ''
-    firstNum = ''
-    secondNum = ''
-    oprat = ''
-    
+   prevNum = ''
+   currentNum = ''
+   operatorDisplay = ''
+   displayInput.textContent = currentNum
+ 
+})
+
+deleteNum.addEventListener('click', (e)=>{
+    currentNum = currentNum.slice(0, -1)
+    displayInput.textContent = currentNum
 })
 
 function handleNumber(num) {
-    firstNum += num
-}
-
-function handleOperator(num){
-    oprat += num;
-    firstNum = secondNum
-    secondNum = ''
-}
-
-function opration(num1,oprat, num2) {
-    switch (oprat) {
-        case '+':
-            return +num1 + +num2
-            break;
-        case '-':
-            return num1 - num2
-            break;
-        case '*':
-            return num1 * num2
-            break;
-        case '/':
-            return num1 / num2
-            break;
+    if(currentNum.length <= 10) {
+    currentNum += num
     }
 }
+
+function handleOperator(op){
+        operatorDisplay = op
+        prevNum = currentNum
+        currentNum = ''
+}
+
+function operation(num1,operatorDisplay, num2) {
+    num1 = Number(num1) 
+    num2 = Number(num2) 
+    switch (operatorDisplay) {
+        case '+':
+            return (num1 += num2)
+            break;
+        case '-':
+            return num2 -= num1
+            break;
+        case '*':
+            return num1 *= num2
+            break;
+        case '/':
+            if(num2 === 0) {
+                return '0'
+            }else {
+                return num1 /= num2
+            }
+            break;
+    }
+    num1 = num1.toString()
+    num2 = num1.toString()
+}
+
+console.log(operation('5','-','2'))
